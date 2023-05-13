9375e1f458bab1a0901351333ee6e71732f34252
MSM74588 <msm74588@gmail.com>
2023-05-13 12:08:33 +0530

    added filesview with items

--- src/FilesView.js
+++ src/FilesView.js
@@ -1,10 +1,47 @@
 import GObject from 'gi://GObject';
 import Gtk from 'gi://Gtk';
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
+
+import { File } from './File.js';
 
 export const FilesView = GObject.registerClass({
     GType: 'FbrFilesView',
-    Template: 'resource:///org/example/filebrowser/ui/FilesView.ui'
+    Template: 'resource:///org/example/filebrowser/ui/FilesView.ui',
+    Properties: {
+		files: GObject.ParamSpec.object(
+			'files',
+			'Files',
+			'The list model containing the files',
+			GObject.ParamFlags.READWRITE,
+			Gio.ListStore
+		),
+	},
 }, class extends Gtk.Widget {
+    constructor(params={}) {
+		super(params);
+		this.#initializeFiles();
+	}
 
-    
+	#initializeFiles() {
+		// Create the Gio.ListStore that will contain File objects
+		this.files = Gio.ListStore.new(File);
+
+		// Get the current directory
+		const currentDir = Gio.File.new_for_path(GLib.get_current_dir());
+
+		// Get an enumerator of all children
+		const children = currentDir.enumerate_children('standard::*', Gio.FileQueryInfoFlags.NOFOLLOW_SYMLINKS, null);
+
+		// Iterate over the enumerator and add each child to the list store
+		let fileInfo;
+		while (fileInfo = children.next_file(null)) {
+			this.files.append(new File({
+				name: fileInfo.get_display_name(),
+				icon: Gio.content_type_get_icon(fileInfo.get_content_type()),
+				type: fileInfo.get_file_type(),
+			}));
+		}
+	}
+
 })
